28c79b66ac6e7bb658845cfe0167ab3e832b5596
Abhishek Nath <abhi.nath@outlook.com>
2023-07-27 12:08:25 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+console.log("=============================================\n");
+console.log("                 Hello World !!!\n");
+console.log("=============================================\n");
